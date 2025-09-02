6b07abe58c8ee6234620736504582f3c9926877c
Andre Menezes <djdedemenezes@gmail.com>
2025-09-02 14:52:43 -0300

    feat: add noticias show with locale

---
+++ app/frontend/pages/Noticias/Show.vue
@@ -0,0 +1,128 @@
+<script setup>
+import TwContainer from "@components/layout/TwContainer.vue"
+import { IconLink } from "@components/common/icons";
+import { ButtonText } from "@components/common/buttons"
+import { ref, computed } from "vue";
+
+const props = defineProps({
+  conteudo: { type: String, required: true, default: '' },
+  titulo: { type: String, required: true, default: '' },
+  chamada: { type: String, required: true, default: '' },
+})
+
+const copyURL = () => {
+  const url = window.location.href; // Get the current URL
+
+  // Use the Clipboard API to copy the URL to the clipboard
+  navigator.clipboard.writeText(url).then(() => {
+    alert('URL copied to clipboard!'); // Show success message
+  }).catch(err => {
+    console.error('Failed to copy URL: ', err);
+    alert('Failed to copy URL!');
+  });
+}
+
+const isHovered = ref(false);
+const handleMouseEnter = () => {
+  isHovered.value = true;
+};
+const handleMouseLeave = () => {
+  isHovered.value = false;
+};
+
+const isFocused = ref(false);
+const handleFocus = () => {
+  isFocused.value = true;
+};
+const handleBlur = () => {
+  isFocused.value = false;
+};
+
+const isActive = computed(() => isHovered.value || isFocused.value);
+
+// const currentRoute = useRoute();
+// const isRouteActive = computed(() => currentRoute.path == props.route);
+const isIconActive = computed(() => isActive.value);
+
+</script>
+
+<template>
+  <TwContainer>
+    <div class="max-w-[940px] mx-auto">
+      <div class="flex flex-col gap-600 mb-800">
+        <h1 class="text-header-lg">{{ props.titulo }}</h1>
+        <p class="text-body-regular-lg">{{ props.chamada }}</p>
+        <p class="flex items-center gap-[8px] text-overline text-neutrals-900"><span class="date">22.07.25</span> <img style="height: 16px;" src="@assets/icons/divisor.svg" alt="Divisor"> <span class="caderno">ESTREIA DA SEMANA</span></p>
+        <ButtonText
+          @click="copyURL"
+          tag="button"
+          text="Copiar URL"
+          variant="dark"
+          size="sm"
+          class="cursor-pointer hover:text-neutrals-700"
+          font-weight="font-regular"
+          @mouseenter="handleMouseEnter"
+          @mouseleave="handleMouseLeave"
+          @focus="handleFocus"
+          @blur="handleBlur"
+        >
+          <template v-slot:icon>
+            <IconLink :active="isIconActive" class="me-100" />
+          </template>
+        </ButtonText>
+      </div>
+      <div v-html="props.conteudo" class="content text-justify text-neutrals-900"></div>
+    </div>
+  </TwContainer>
+</template>
+
+<style scoped>
+::v-deep .content p, ::v-deep .content span, ::v-deep .content b {
+  /* Body/Regular/Body Double Space Regular medium */
+  font-family: "Inter" !important;
+  font-size: 16px !important;
+  font-style: normal !important;
+  font-weight: 400 !important;
+  line-height: 200% !important; /* 32px */
+}
+
+::v-deep .content p:not(:empty):not(:has(img)) {
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .content b {
+  font-weight: 600 !important;
+}
+
+::v-deep .content a {
+  text-decoration: underline !important;
+}
+
+::v-deep .content a:hover {
+  color: var(--color-vermelho-400) !important; /* Using the custom variable */
+}
+
+::v-deep .content img {
+  width: 100% !important;
+  border-radius: 8px;
+}
+
+::v-deep .content img + br + i:not(:empty), ::v-deep .content i > span, ::v-deep .content span > i  {
+  /* Body/Regular/Body Regular small */
+  font-family: "Inter" !important;
+  font-size: 14px !important;
+  font-style: normal !important;
+  font-weight: 400 !important;
+  line-height: 150% !important; /* 21px */
+  color: #3B3935 !important;
+}
+
+::v-deep p:has(i) {
+  margin-bottom: 1rem;
+}
+
+::v-deep br {
+  display: none;
+}
+
+</style>
