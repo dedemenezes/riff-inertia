780b68218f81f1d61b8112fd8ba0174c6bcd5597
Andre Menezes <djdedemenezes@gmail.com>
2025-09-03 13:35:47 -0300

    feat: add shadcn and input component

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import path from "path"
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      '@': fileURLToPath(new URL('./app/frontend', import.meta.url)),
+      '@/components': fileURLToPath(new URL('./app/frontend/components', import.meta.url)),
+      '@/lib': fileURLToPath(new URL('./app/frontend/lib', import.meta.url)),
+      '@/pages': fileURLToPath(new URL('./app/frontend/pages', import.meta.url)),
+      '@/assets': fileURLToPath(new URL('./app/frontend/assets', import.meta.url)),
       "@assets": path.resolve(__dirname, "app/frontend/assets"),
       "@components": path.resolve(__dirname, "app/frontend/components"),
       "@pages": path.resolve(__dirname, "app/frontend/pages"),
