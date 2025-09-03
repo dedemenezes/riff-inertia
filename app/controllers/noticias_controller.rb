ea70245bee886696cd89406364e81c4489dc2b16
Andre Menezes <djdedemenezes@gmail.com>
2025-09-03 19:00:35 -0300

    feat: add noticias indez controller

--- app/controllers/noticias_controller.rb
+++ app/controllers/noticias_controller.rb
@@ -1,6 +1,23 @@
 class NoticiasController < ApplicationController
   include BreadcrumbsHelper
 
+  def index
+    # TODO: Utilizar paginação ou automatic scroll
+    @noticias = Noticia.includes(:caderno).order(created: :desc).first(10)
+
+    render inertia: "Noticias/Index", props: {
+      rootUrl: @root_url,
+      breadcrumbs: breadcrumbs(
+        [ "", @root_url ],
+        [ "Notícias", "" ],
+      ),
+      noticias: @noticias.as_json(
+                only: %i[id titulo permalink chamada imagem],
+                methods: [ :caderno_nome, :display_date ]
+              )
+    }
+  end
+
   def show
     @noticia = Noticia.find_by_permalink(params[:permalink])
     # binding.b
