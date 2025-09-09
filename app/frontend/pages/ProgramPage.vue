10cf3f3aa4f6f99093fee5103e1945181c903466
Andre Menezes <djdedemenezes@gmail.com>
2025-09-09 09:47:05 -0300

    feat: integrate new filtering system into program page
    - Replace separate mobile/desktop filter triggers with unified ResponsiveFilterMenu
    - Implement sticky menu tabs with sentinel-based detection
    - Update layout to use CSS Grid for filter sidebar

--- app/frontend/pages/ProgramPage.vue
+++ app/frontend/pages/ProgramPage.vue
@@ -1,17 +1,22 @@
 <script setup>
-import { ref } from "vue";
+// TODO: CHANGE TEXT WHEN NO RESULT FOR FILTERING
+import { ref,  watch } from "vue";
 import { router } from "@inertiajs/vue3"
 import { IconProgram, IconClock, IconChange, IconNewUser } from "@components/common/icons";
-import MenuContext from "@/components/layout/navbar/MenuContext.vue";
-import SessionCard from "@/components/common/cards/SessionCard.vue";
-import InfiniteScrollLayout from "@/components/layout/InfiniteScrollLayout.vue";
+
 import TwContainer from "@/components/layout/TwContainer.vue";
+import InfiniteScrollLayout from "@/components/layout/InfiniteScrollLayout.vue";
+import MenuContext from "@/components/layout/navbar/MenuContext.vue";
 import MenuTabs from "@/components/layout/navbar/MenuTabs.vue";
-import SearchBar from "@/components/features/filters/SearchBar.vue";
+
 import MobileTrigger from "@/components/features/filters/MobileTrigger.vue";
-import { useMobileTrigger } from "@/components/features/filters/composables/useMobileTrigger";
-import MobileFilterMenu from "@/components/features/filters/MobileFilterMenu.vue";
 import ProgramsFilterForm from "@/components/features/filters/ProgramsFilterForm.vue";
+import SessionCard from "@/components/common/cards/SessionCard.vue";
+
+import { useMobileTrigger } from "@/components/features/filters/composables/useMobileTrigger";
+import { useStickyMenuTabs } from "@/components/layout/navbar/composables/useStickyMenuTabs";
+
+import ResponsiveFilterMenu from "@/components/features/filters/ResponsiveFilterMenu.vue";
 
 const { isFilterMenuOpen, openMenu, closeMenu } = useMobileTrigger();
 
@@ -20,34 +25,15 @@ const props = defineProps({
   items: { type: Array, required: true }
   ,elements: { type: Object, required: true }
   ,pagy: { type: Object, required: true }
-  ,available_dates: { type: Array }
-  ,selected_date: { type: String }
-  ,searchQuery: { type: String }
-  ,selectedFilters: { type: Object, default: () => ({})}
   ,mostrasFilter: { type: Array, default: () => [] }
+  // NEW LIFE
+  ,menuTabs: { type: Array, required: true }
+  ,current_filters: { type: Object, default: () => ({}) }
+  ,has_active_filters: { type: Boolean, default: false }
 })
 
-const searchQuery = ref(props.searchQuery)
+const localFilters = ref({ ...props.current_filters })
 
-const handleSearch = () => {
-  const query = `query=${searchQuery.value}`
-  console.log(query);
-  router.visit(`${props.tabBaseUrl}?${query}`, {
-    preserveScroll: true,
-    preserveState: true,
-    only: ["elements", "searchQuery", "available_dates", "selected_date"],
-  });
-}
-const handleClear = () => {
-  const url = new URL(window.location.href)
-  const params = url.searchParams
-  params.delete("query") // TODO: hardcode query params name
-    router.visit(props.tabBaseUrl, {
-    preserveScroll: true,
-    preserveState: true,
-    only: ["elements", "searchQuery", "available_dates"],
-  });
-}
 
 const iconMapping = {
   "program": IconProgram,
@@ -56,30 +42,39 @@ const iconMapping = {
   "clock": IconClock
 };
 
+
+// Called when user clears search bar
+const handleClear = () => {
+  filters.value.query = null;
+  submit(props.tabBaseUrl);
+};
+
+// Called when filters applied in MobileFilterMenu
 const filterSearch = (filtersFromChild) => {
-  console.log("filterSearch")
-  console.log(filtersFromChild)
-  // Build query params
-  // Build full url
-  const params = new URLSearchParams();
+  const cleanedFilters = {}
+
   for (const [key, value] of Object.entries(filtersFromChild)) {
-    if (value?.tag_class) {
-      params.append(key, value.tag_class);
+    if (key === 'mostrasFilter' && typeof value === 'object' && value?.permalink_pt) {
+      cleanedFilters[key] = value.permalink_pt  // Extract just the string
+    } else {
+      cleanedFilters[key] = value
     }
   }
-  // debugger
-  // Submit request
-  router.visit(`${props.tabBaseUrl}?${params.toString()}`, {
-    preserveScroll: true,
+  router.get(props.tabBaseUrl, cleanedFilters, {
     preserveState: true,
-    // Select props to update
-    only: ["elements", "selectedFilters"],
-  });
-  // let it go
-}
+    preserveScroll: true,
+    only: ['elements', 'pagy', 'current_filters', 'has_active_filters', 'menuTabs']
+  })
+};
+
+// Called when filters cleared from MobileFilterMenu
 const clearSearchQuery = () => {
-  console.log("clearSearchQuery")
-}
+  clearAll();
+  submit(props.tabBaseUrl);
+};
+
+// sticket menutabs
+const { sentinel, isSticky } = useStickyMenuTabs()
 </script>
 
 <template>
@@ -87,72 +82,49 @@ const clearSearchQuery = () => {
   :items="props.items"
   :icon-mapping="iconMapping"
   />
-  <TwContainer>
-    <SearchBar
-      v-model="searchQuery"
-      @search="handleSearch"
-      @clear="handleClear"
+  <TwContainer class="relative">
+    <!-- Added for sticky menutabs -->
+    <div ref="sentinel" class="h-1"></div>
+    <MenuTabs
+      :is-sticky="isSticky"
+      :tabs="menuTabs"
+      class="h-15"
     />
-
     <div
-      class="filter flex items-center justify-between md:gap-800 lg:gap-1200 py-300"
+    class="filter flex items-center justify-end py-300 sticky top-15 z-40 bg-white"
     >
       <MobileTrigger @open-menu="openMenu" />
-
-      <!-- Ordering -->
-      <div class="flex items-center gap-300">
-        <span class="text-body-strong-sm uppercase text-secondary-gray"
-          >A - Z</span
-        >
-        <img
-          src="@assets/icons/divisor.svg"
-          alt="divisor"
-          height="16px"
-          width="1px"
-        />
-        <span class="text-body-strong-sm uppercase text-neutrals-900">
-          <!-- {{$t("filter_by.date")}} -->
-           por Data
-        </span>
-      </div>
-      <!-- Ordering -->
     </div>
-    <!-- mobile filter content -->
-    <transition name="slide-left">
-      <MobileFilterMenu
-        :is-open="isFilterMenuOpen"
-        :initialFilters="props.selectedFilters"
-        @filtersApplied="filterSearch"
-        @filtersCleared="clearSearchQuery"
-        @close-filter-menu="closeMenu"
-      >
-        <template #filters="{ modelValue, updateField }">
-          <ProgramsFilterForm
-            :model-value="modelValue"
-            :update-field="updateField"
-            :mostrasFilter="props.mostrasFilter"
+
+    <div class="grid grid-cols-12">
+      <div class="col-span-12 md:col-span-6">
+        <InfiniteScrollLayout #content="{ allElements }"
+          :elements="props.elements"
+          :pagy="props.pagy"
+        >
+          <SessionCard v-for="session in allElements"
+            :session="session"
           />
-          <!-- {{ modelValue }} -->
-        </template>
-      </MobileFilterMenu>
-        <!-- input -->
-    </transition>
-    <!-- mobile filter content -->
-    <MenuTabs
-    :collection="props.available_dates"
-    :tabBaseUrl="props.tabBaseUrl"
-    :current-query="searchQuery"
-    :selected-filters="props.selectedFilters"
-    />
-    <h1 class="text-header-medium-sm py-400 text-neutrals-900">{{ selected_date }}</h1>
-    <InfiniteScrollLayout #content="{ allElements }"
-      :elements="props.elements"
-      :pagy="props.pagy"
-    >
-      <SessionCard v-for="session in allElements"
-        :session="session"
-      />
-    </InfiniteScrollLayout>
+        </InfiniteScrollLayout>
+      </div>
+      <div class="col-start-8 col-span-6 sticky top-0">
+        <ResponsiveFilterMenu
+          :is-open="isFilterMenuOpen"
+          :initialFilters="localFilters"
+          @filtersApplied="filterSearch"
+          @filtersCleared="clearSearchQuery"
+          @close-filter-menu="closeMenu"
+        >
+          <template #filters="{ modelValue, updateField }">
+            <ProgramsFilterForm
+              :model-value="modelValue"
+              :update-field="updateField"
+              :mostrasFilter="props.mostrasFilter"
+            />
+          </template>
+        </ResponsiveFilterMenu>
+      </div>
+    </div>
   </TwContainer>
 </template>
 
