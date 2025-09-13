f32d78d326fbae6b5672b6eb9e7b7cdeac0ed222
Luca Severo <luca98.severo@gmail.com>
2025-09-13 01:40:37 -0300

    Feature: Peliculas#index without the search and filters

--- app/controllers/peliculas_controller.rb
+++ app/controllers/peliculas_controller.rb
@@ -1,6 +1,24 @@
 class PeliculasController < ApplicationController
   include BreadcrumbsHelper
 
+  def index
+    @edicao_atual = Edicao.find_by(descricao: ApplicationRecord::EDICAO_ATUAL_ANO)
+    @peliculas = @edicao_atual.peliculas.order(titulo_portugues_coord_int: :asc)
+
+    render inertia: "Peliculas/Index", props: {
+      rootUrl: @root_url,
+      peliculas: @peliculas.as_json(
+        only: Pelicula::COLUMNS_NEEDED,
+        methods: Pelicula::METHODS_NEEDED
+      ),
+      crumbs: breadcrumbs(
+        [ "", @root_url ],
+        [ I18n.t("navigation.edition2024"), "" ],
+        [ "Todos os Filmes", peliculas_path ]
+      )
+    }
+  end
+
   def show
     @pelicula = Pelicula.find_by(edicao_id: 12, permalink: params[:permalink])
     render inertia: "Peliculas/Show", props: {
---
+++ app/frontend/pages/Peliculas/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MenuContext from "@/components/layout/navbar/MenuContext.vue";
+import TwContainer from "@/components/layout/TwContainer.vue";
+import Breadcrumb from "@/components/common/Breadcrumb.vue";
+import PeliculaCard from "@/components/common/cards/PeliculaCard.vue";
+
+
+const props = defineProps({
+  peliculas: { type: Array, default: () => []},
+  crumbs: { type: Array, required: true, default: () => []}
+})
+
+</script>
+
+<template>
+  <TwContainer>
+    <Breadcrumb :crumbs="props.crumbs" />
+  </TwContainer>
+  <MenuContext
+    nav="edicao"
+  />
+  <hr class="text-neutrals-300">
+
+  <TwContainer>
+    <ul class="grid grid-cols-2 lg:grid-cols-3 py-1200 gap-800">
+      <PeliculaCard
+        v-for="pelicula in props.peliculas"
+        :pelicula="pelicula"
+      />
+    </ul>
+  </TwContainer>
+</template>
+
+<style scoped></style>
