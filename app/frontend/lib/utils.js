4a1e89b6249d416c402b7001b7a23fc50953d4b2
Andre Menezes <djdedemenezes@gmail.com>
2025-09-15 13:24:14 -0300

    feat: add js utils for simple translations and formatted time string

--- app/frontend/lib/utils.js
+++ app/frontend/lib/utils.js
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ref, computed, onMounted, onUnmounted } from "vue";
+import { usePage } from "@inertiajs/vue3";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -36,4 +37,32 @@ export function useUpdateWindowWidth() {
   onUnmounted(() => window.removeEventListener('resize', updateWidth))
 
   return isDesktop
+}
+
+export function displayFormattedTime(time) {
+    const page = usePage();
+
+    const locale = page.props.currentLocale  // or inject it however you're doing it
+
+    // Convert "14h30" → "14:30"
+    const timeStr = time.replace('h', ':');
+
+    // Create a Date object with today's date and the session time
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    const date = new Date();
+    date.setHours(hours, minutes, 0, 0);
+
+    // Format the time
+    const formattedTime = new Intl.DateTimeFormat(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+    }).format(date);
+
+    return formattedTime
+}
+
+export function simpleTranslation(ptMessage, enMessage) {
+  const page = usePage();
+  const locale = page.props.currentLocale
+  return locale == "pt" ? ptMessage : enMessage
 }
