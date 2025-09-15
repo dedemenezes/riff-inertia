2424a98a00ff0fd4d0ec1f3ba2e94b11d2acc6a5
Andre Menezes <djdedemenezes@gmail.com>
2025-09-14 21:37:14 -0300

    feat add infinite_scrollable concern

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,4 +1,6 @@
 class ProgramsController < ApplicationController
+  include InfiniteScrollable
+
   EDICAO_ATUAL = 12
   DATES_PER_PAGE = 1
   # TODO: MAKE THIS ENV VARIABLE?
@@ -203,36 +205,6 @@ class ProgramsController < ApplicationController
   end
 
   private
-
-  # TODO: Relocate this piece into a concern or helper
-  def pagy_infinite(collection, page_param)
-    current_page = (page_param || params[:page] || 1).to_i
-    limit = Pagy::DEFAULT[:limit] || 5
-
-    if current_page <= 1
-      # First page - normal pagination
-      pagy_result = pagy(collection, limit: limit)
-      pagy_result
-    else
-      # Infinite scroll - load all items from page 1 to current page
-      total_items_needed = current_page * limit
-
-      # Get the actual items
-      items = collection.limit(total_items_needed)
-
-      # Create proper pagy object with
-      # all the metadata need in the frontend
-      total_count = collection.count
-      pagy_obj = Pagy.new(
-        count: total_count,
-        limit: limit,
-        page: current_page,
-        # overflow: :last_page
-      )
-
-      [ pagy_obj, items ]
-    end
-  end
 
   def build_tab_url(date, filters)
     query_params = {}
