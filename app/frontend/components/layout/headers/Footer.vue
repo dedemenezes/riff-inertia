01248a95cb4174dd8676f09a24d1bff9a716aa7e
Luca Severo <luca98.severo@gmail.com>
2025-09-16 17:07:16 -0300

    Feature: Footer added; missing links to inexisting pages

--- app/frontend/components/layout/headers/Footer.vue
+++ app/frontend/components/layout/headers/Footer.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { IconInstagram, IconFacebook, IconTikTok, IconYoutube, IconLetterboxd, IconX, IconThreads, IconBlueSky, IconFlickr } from '@/components/common/icons/';
 import TwContainer from '@/components/layout/TwContainer.vue';
+import { usePage } from "@inertiajs/vue3";
 
+const page = usePage()
+const locale = page.props.currentLocale
 
 </script>
 
 <template>
 
-  <div class="bg-neutrals-200 py-1200">
+  <div class="bg-neutrals-200 py-1200 text-neutrals-900">
     <TwContainer class="flex flex-col gap-1200">
       <div class="flex justify-end gap-1200">
         <a href="https://www.instagram.com/festivaldorio/">
@@ -64,6 +67,27 @@ import TwContainer from '@/components/layout/TwContainer.vue';
           />
         </a>
       </div>
+      <div>
+        <!-- TODO: Add rotas!!!!! -->
+        <div class="flex gap-1200 text-body-regular border-b-1 py-300">
+          <a :href="`/${locale}/`" class="p-200">O Festival</a>
+          <a :href="`/${locale}/edicoes_anteriores`" class="p-200">Edições Anteriores</a>
+          <a :href="`/${locale}/`" class="p-200">Talents Rio</a>
+          <a :href="`/${locale}/`" class="p-200">Parceiros</a>
+        </div>
+        <div class="flex gap-1200 text-body-regular border-b-1 py-300">
+          <a :href="`/${locale}/`" class="p-200">Fotos & Vídeos</a>
+          <a :href="`/${locale}/`" class="p-200">Impressos</a>
+        </div>
+        <div class="flex gap-1200 text-body-regular border-b-1 py-300">
+          <a :href="`/${locale}/`" class="p-200">Ingressos</a>
+          <a :href="`/${locale}/`" class="p-200">Imprensa</a>
+          <a :href="`/${locale}/`" class="p-200">Acessibilidade</a>
+          <a :href="`/${locale}/`" class="p-200">Contatos</a>
+        </div>
+      </div>
+
+      <h4 class="text-overline text-center">COPYRIGHT © FESTIVAL DO RIO 2025. TODOS OS DIREITOS RESERVADOS.</h4>
     </TwContainer>
   </div>
 </template>
