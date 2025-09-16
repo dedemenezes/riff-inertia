4058254b1602ec4a168e6c7bc01a69291e829d8a
Andre Menezes <djdedemenezes@gmail.com>
2025-09-16 15:55:22 -0300

    add next sessions horizontal scroling

--- app/frontend/pages/HomePage.vue
+++ app/frontend/pages/HomePage.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
-import { defineAsyncComponent } from "vue";
+import { ref, defineAsyncComponent, computed } from "vue";
 import TwContainer from "@components/layout/TwContainer.vue"
 
 import HomeBanner from '@/components/features/home/HomeBanner.vue';
 import banneImagePath from "@assets/images/mobile-banner.png";
-
+import { IconChevronRight, IconChevronLeft } from "@/components/common/icons"
 import QuickLinkSection from '@components/common/cards/QuickLinkSection.vue';
 import ArticlesSection from '@components/features/home/ArticlesSection.vue';
 import NavbarSecondary from "@components/layout/navbar/NavbarSecondary.vue";
+import SessionCard from "@/components/common/cards/SessionCard.vue";
+import { useUpdateWindowWidth } from "@/lib/utils";
 const TvFestival = defineAsyncComponent(() => import("@/components/layout/TvFestival.vue"));
 
 const props = defineProps({
@@ -17,8 +19,24 @@ const props = defineProps({
   secondaryItems: { type: Array },
   noticias: { type: Array },
   noticasUrl: { type: String },
-  youtubeVideos: { type: Array }
+  youtubeVideos: { type: Array },
+  nextSessions: { type: Array }
 })
+
+const isDesktop = useUpdateWindowWidth()
+
+const scrollContainer = ref(null);
+
+const scrollLeft = () => {
+  scrollContainer.value.scrollBy({ left: -250, behavior: 'smooth' });
+};
+
+
+const width = ref(typeof window !== 'undefined' ? window.innerWidth : 0)
+
+const scrollRight = () => {
+  scrollContainer.value.scrollBy({ left: width.value, behavior: 'smooth' });
+};
 </script>
 
 <template>
@@ -33,6 +51,37 @@ const props = defineProps({
   </HomeBanner>
   <QuickLinkSection v-bind:links="quickLinksConfig" />
 
+  <!-- <div
+    ref="scrollContainer"
+    role="tablist"
+    class="flex items-center gap-300 overflow-x-auto no-scroll-bar py-300 lg:py-400"
+  > -->
+  <div class="relative">
+    <div ref="scrollContainer" class="flex overflow-x-auto no-scroll-bar gap-800 pb-4">
+      <div class="flex-shrink-0 w-[calc(80%-0.75rem)] sm:w-[calc(30%-0.75rem)] sm:min-w-[300px]"
+           :class="{ 'ml-4 lg:ml-45': index === 0, 'mr-4': index === props.nextSessions.length - 1 }"
+           v-for="(session, index) in props.nextSessions" :key="session.id">
+        <SessionCard :session="session" />
+      </div>
+    </div>
+    <button
+      @click="scrollLeft"
+      class="absolute left-400 top-1/3 -translate-y-1/3 z-10 bg-transparent hover:text-white-neutrals-900"
+      aria-label="Scroll right"
+    >
+      <IconChevronLeft class="w-6 h-6 text-white-transp-800" />
+    </button>
+    <button
+      @click="scrollRight"
+      class="absolute right-400 top-1/3 -translate-y-1/3 z-10 bg-transparent hover:text-white-transp-1000"
+      aria-label="Scroll right"
+    >
+      <IconChevronRight class="w-6 h-6 text-white-transp-800" />
+    </button>
+  </div>
+  <!-- </div> -->
+  <TwContainer>
+  </TwContainer>
   <TwContainer>
     <div class="py-1200">
       <ArticlesSection :articles="props.noticias" :noticiasUrl="props.noticasUrl"/>
@@ -42,4 +91,11 @@ const props = defineProps({
   <TvFestival :youtube-videos="props.youtubeVideos" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-out--right {
+  -webkit-mask-image: linear-gradient(to right, black 0%, black 85%, transparent 100%);
+  mask-image: linear-gradient(to right, black 0%, black 85%, transparent 100%);
+  -webkit-mask-size: 100% 100%;
+  mask-size: 100% 100%;
+}
+</style>
