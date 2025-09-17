4486b47d307248e56f9957937b5cadf67276c686
Andre Menezes <djdedemenezes@gmail.com>
2025-09-16 23:03:42 -0300

    feat add horizontal scroll component

---
+++ app/frontend/components/layout/HorizontalScrollLayout.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+import TwContainer from "./TwContainer.vue";
+import { IconChevronRight, IconChevronLeft } from "@/components/common/icons"
+
+const scrollContainer = ref(null);
+const width = ref(typeof window !== 'undefined' ? window.innerWidth : 0)
+const scrollLeft = () => {
+  scrollContainer.value.scrollBy({ left: -width.value*0.80, behavior: 'smooth' });
+};
+const scrollRight = () => {
+  scrollContainer.value.scrollBy({ left: width.value*0.80, behavior: 'smooth' });
+};
+</script>
+
+<template>
+  <div class="relative flex flex-col gap-600">
+    <TwContainer>
+      <slot name="header" />
+    </TwContainer>
+    <div ref="scrollContainer" class="flex overflow-x-auto no-scroll-bar gap-800">
+      <slot name="content" />
+    </div>
+    <button
+      @click="scrollLeft"
+      class="absolute left-400 top-1/2 -translate-y-1/2 z-10 bg-transparent hover:text-white-neutrals-900"
+      aria-label="Scroll right"
+    >
+      <IconChevronLeft class="w-6 h-6 text-white-transp-800" />
+    </button>
+    <button
+      @click="scrollRight"
+      class="absolute right-400 top-1/2 -translate-y-1/2 z-10 bg-transparent hover:text-white-transp-1000" aria-label="Scroll right"
+    >
+      <IconChevronRight class="w-6 h-6 text-white-transp-800" />
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
--- app/frontend/pages/HomePage.vue
+++ app/frontend/pages/HomePage.vue
@@ -5,13 +5,15 @@ import TwContainer from "@components/layout/TwContainer.vue"
 
 import HomeBanner from '@/components/features/home/HomeBanner.vue';
 import banneImagePath from "@assets/images/mobile-banner.png";
-import { IconChevronRight, IconChevronLeft } from "@/components/common/icons"
 import QuickLinkSection from '@components/common/cards/QuickLinkSection.vue';
 import ArticlesSection from '@components/features/home/ArticlesSection.vue';
 import NavbarSecondary from "@components/layout/navbar/NavbarSecondary.vue";
 import SessionCard from "@/components/common/cards/SessionCard.vue";
-import { simpleTranslation, useUpdateWindowWidth } from "@/lib/utils";
+import HorizontalScrollLayout from "@/components/layout/HorizontalScrollLayout.vue";
 const TvFestival = defineAsyncComponent(() => import("@/components/layout/TvFestival.vue"));
+
+import { simpleTranslation } from "@/lib/utils";
+
 
 const props = defineProps({
   quickLinksConfig: { type: Array },
@@ -23,20 +25,6 @@ const props = defineProps({
   nextSessions: { type: Array }
 })
 
-const isDesktop = useUpdateWindowWidth()
-
-const scrollContainer = ref(null);
-
-const scrollLeft = () => {
-  scrollContainer.value.scrollBy({ left: -250, behavior: 'smooth' });
-};
-
-
-const width = ref(typeof window !== 'undefined' ? window.innerWidth : 0)
-
-const scrollRight = () => {
-  scrollContainer.value.scrollBy({ left: width.value*0.80, behavior: 'smooth' });
-};
 </script>
 
 <template>
@@ -51,32 +39,19 @@ const scrollRight = () => {
   </HomeBanner>
   <QuickLinkSection v-bind:links="quickLinksConfig" />
 
+
   <div class="py-1600">
-    <div class="relative flex flex-col gap-600">
-      <TwContainer>
+    <HorizontalScrollLayout>
+      <template v-slot:header>
         <h3 class="text-header-xxl text-neutrals-900">{{ simpleTranslation("Próximas sessões", "Upcoming sessions") }}</h3>
-      </TwContainer>
-      <div ref="scrollContainer" class="flex overflow-x-auto no-scroll-bar gap-800">
+      </template>
+      <template v-slot:content>
         <div class="flex-shrink-0 w-[calc(80%-0.75rem)] sm:w-[calc(30%-0.75rem)] sm:min-w-[300px]"
-             v-for="(session, index) in props.nextSessions" :key="session.id">
+          v-for="(session, index) in props.nextSessions" :key="session.id">
           <SessionCard :session="session" />
         </div>
-      </div>
-      <button
-        @click="scrollLeft"
-        class="absolute left-400 top-1/2 -translate-y-1/2 z-10 bg-transparent hover:text-white-neutrals-900"
-        aria-label="Scroll right"
-      >
-        <IconChevronLeft class="w-6 h-6 text-white-transp-800" />
-      </button>
-      <button
-        @click="scrollRight"
-        class="absolute right-400 top-1/2 -translate-y-1/2 z-10 bg-transparent hover:text-white-transp-1000"
-        aria-label="Scroll right"
-      >
-        <IconChevronRight class="w-6 h-6 text-white-transp-800" />
-      </button>
-    </div>
+      </template>
+    </HorizontalScrollLayout>
   </div>
   <!-- </div> -->
   <TwContainer>
