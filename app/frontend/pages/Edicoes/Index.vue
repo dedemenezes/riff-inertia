5316bde7c715b2b01b5841d5ad6846ff95f9901f
Andre Menezes <djdedemenezes@gmail.com>
2026-05-07 00:44:25 -0300

    RIFF-52: refactor index to use new cartaz div component

--- app/frontend/pages/Edicoes/Index.vue
+++ app/frontend/pages/Edicoes/Index.vue
@@ -3,30 +3,31 @@ import MenuContext from "@/components/layout/navbar/MenuContext.vue";
 import TwContainer from "@/components/layout/TwContainer.vue";
 import Cartaz from "@/components/common/cards/Cartaz.vue";
 import Breadcrumb from "@/components/common/Breadcrumb.vue";
+import { Link } from "@inertiajs/vue3";
 
 const props = defineProps({
-  edicoes: { type: Array, default: () => []},
-  crumbs: { type: Array, required: true}
-})
-
+  edicoes: { type: Array, default: () => [] },
+  crumbs: { type: Array, required: true },
+});
 </script>
 
 <template>
   <TwContainer>
     <Breadcrumb :crumbs="props.crumbs" />
   </TwContainer>
-  <MenuContext
-    nav="sobre"
-  />
-  <hr class="text-neutrals-300">
+  <MenuContext nav="sobre" />
+  <hr class="text-neutrals-300" />
 
   <TwContainer>
     <ul class="grid grid-cols-2 lg:grid-cols-6 py-1200 gap-800">
-      <Cartaz
-        v-for="edicao in props.edicoes" :key="edicao.id"
-        :cartaz="`https://s3.amazonaws.com/festivaldorio/imagens/edicoes/small/${edicao.cartaz}`"
-        :descricao="edicao.descricao"
-      />
+      <li v-for="edicao in props.edicoes" :key="edicao.id">
+        <Link :href="`/edicoes_anteriores/${edicao.id}/mostras`" prefetch>
+          <Cartaz
+            :cartaz="`https://s3.amazonaws.com/festivaldorio/imagens/edicoes/small/${edicao.cartaz}`"
+            :descricao="edicao.descricao"
+          />
+        </Link>
+      </li>
     </ul>
   </TwContainer>
 </template>
